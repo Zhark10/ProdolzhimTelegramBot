893291f91ef8e40303213b857b241cc2becd2c2c
Zhark10 <verygoodman18@yandex.ru>
2021-12-07 23:56:19 +0300

    minor change

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const TelegramApi = require('node-telegram-bot-api');
 
-const TOKEN = '5035331349:AAHG-3mOMX0gu-1X0bDA31PxTXGQ1akhn1E'
+const token = '5035331349:AAHG-3mOMX0gu-1X0bDA31PxTXGQ1akhn1E'
 
+const bot = new TelegramApi(token, { polling: true })
+
+bot.on('message', (msg) => {
+  const text = msg.text
+  const chatId = msg.chat.id
+  bot.sendMessage(chatId, `Ты написал мне ${text}`)
+  console.log(msg)
+})
