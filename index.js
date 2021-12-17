687e831cae004f8106b22de3d1ac4675e25c39c8
Zhark10 <verygoodman18@yandex.ru>
2021-12-17 21:21:17 +0300

    create history started

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-import TelegramApi from 'node-telegram-bot-api';
-import { join, dirname } from 'path'
-import { Low, JSONFile } from 'lowdb'
-import { fileURLToPath } from 'url'
+import TelegramApi from "node-telegram-bot-api";
+import { join, dirname } from "path"
+import { Low, JSONFile } from "lowdb"
+import { fileURLToPath } from "url"
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -13,7 +13,7 @@ const defaultDatabaseState = {
 }
 
 export async function createConnection() {
-  const file = join(__dirname, 'db.json')
+  const file = join(__dirname, "db.json")
   const adapter = new JSONFile(file)
   db = await new Low(adapter)
   db.data ||= defaultDatabaseState
@@ -23,7 +23,7 @@ export async function createConnection() {
 export const getConnetcion = () => db
 createConnection()
 
-const token = '5035331349:AAHG-3mOMX0gu-1X0bDA31PxTXGQ1akhn1E'
+const token = "5035331349:AAHG-3mOMX0gu-1X0bDA31PxTXGQ1akhn1E"
 
 const bot = new TelegramApi(token, { polling: true })
 
@@ -31,23 +31,30 @@ const start = () => {
 
   bot.setMyCommands(
     [
-      { command: '/start', description: 'Приветствие' },
-      { command: '/create', description: 'Создать историю' },
+      { command: "/start", description: "Приветствие" },
+      { command: "/create", description: "Создать историю" },
     ]
   )
 
-  bot.on('message', async (msg) => {
+  bot.on("message", async (msg) => {
     const text = msg.text
     const chatId = msg.chat.id
 
     if (text === "/start") {
-      await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/e1f/4c4/e1f4c48a-c808-37a7-a037-bfa58e8b3222/1.webp')
+      await bot.sendSticker(chatId, "https://tlgrm.ru/_/stickers/e1f/4c4/e1f4c48a-c808-37a7-a037-bfa58e8b3222/1.webp")
       return bot.sendMessage(chatId, `Привет, ${msg.from.first_name} ${msg.from.last_name}! Добро пожаловать в "Продолжим?"`)
+    }
+
+    if (text === "/create") {
+      await db.data
+        .histories
+        .push({ title: 'Новая история', text: 'История начинается, когда', id: 1 })
+      return
     }
 
     if (text.includes("/continue-")) {
       const storyId = text.split("-")[1]
-      await bot.sendMessage(chatId, storyId || 'Извините, но история не найдена')
+      await bot.sendMessage(chatId, storyId || "Извините, но история не найдена")
     }
 
     return bot.sendMessage(chatId, `Я тебя не понимаю, попробуй объяснить еще раз!`)
