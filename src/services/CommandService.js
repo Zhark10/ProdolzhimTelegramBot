94038348616f50af937162163225ce2bde43902c
Zhark10 <verygoodman18@yandex.ru>
2021-12-18 02:32:51 +0300

    upd history example, add checkingNextHistoryVersion in command service

--- src/services/CommandService.js
+++ src/services/CommandService.js
@@ -14,11 +14,18 @@ export class CommandService {
   }
 
   create = async (msg) => {
-    const chatId = msg.chat.id
     const id = crypto.randomBytes(16).toString("hex");
+    const historyExample = { 
+      title: 'ДВА СТВОЛА', 
+      category: 'Вестерн',
+      text: 'ГЛАВА ПЕРВАЯ. Джо «Злой карлик» был достаточно высок для карлика, он любил пышногрудых дам и изрядно напивался в баре, запугивая бармена своим револьвером с нетипичным количеством слотов в рулетке. Их было два. У Джо был брат, по крайней мере, он так говорил, но об этом позже. Для начала необходимо сказать о том, что «Злым карликом» его прозвали не спроста.', 
+      id 
+    }
+    const chatId = msg.chat.id
     await this.db.data
       .histories
-      .push({ title: 'Новая история', text: 'История начинается, когда', id })
+      .push(historyExample)
+      await this.bot.sendMessage(chatId, `${historyExample.category} «${historyExample.title}»`)
       return this.bot.sendMessage(chatId, `Чтобы продолжить историю, нажми на /continue${CONSTANTS.SEPARATOR_TO_CREATE_UNIQUE_COMMAND}${id}`)
   }
 
@@ -29,15 +36,25 @@ export class CommandService {
 
     const historyIdToSearching = text.split(CONSTANTS.SEPARATOR_TO_CREATE_UNIQUE_COMMAND)[1]
     const historyIndex = await allHistories.findIndex(history => history.id === historyIdToSearching)
-    if (allHistories[historyIndex]?.text) {
-      allHistories[historyIndex].text = allHistories[historyIndex].text + text
+    const isHistoryTextExist = allHistories[historyIndex]?.text
+    if (isHistoryTextExist) {
+      await this.bot.sendMessage(chatId, `Продолжим ${allHistories[historyIndex].category.toLowerCase()} «${allHistories[historyIndex].title}»? :) Скопируй полностью отрывок ниже и дополни следующим сообщением!`)
       return this.bot.sendMessage(chatId, allHistories[historyIndex].text)
     }
-    return this.bot.sendMessage(chatId, "Не удалось дополнить историю")
+    return this.bot.sendMessage(chatId, "К сожалению, история не найдена:( Возможно, она была удалена автором.")
   }
 
-  throwDefaultCase = async (msg) => {
+  checkingNextHistoryVersion = async (msg) => {
+    const text = msg.text
     const chatId = msg.chat.id
-    return this.bot.sendMessage(chatId, `Я тебя не понимаю, попробуй объяснить еще раз!`)
+    const allHistories = this.db.data.histories
+    const historyIndex = await allHistories.findIndex(history => text.includes(history.text))
+    const foundHistory = allHistories[historyIndex]
+    if (foundHistory?.text) {
+      foundHistory.text = text
+      await this.bot.sendMessage(chatId, `Ееее, записано! Теперь перешли другу следующее сообщение`)
+      return this.bot.sendMessage(chatId, `Гоу продолжим историю «${foundHistory.title}»! Просто нажми на t.me/Zhark10Bot и введи /continue${CONSTANTS.SEPARATOR_TO_CREATE_UNIQUE_COMMAND}${foundHistory.id}`)
+    }
+    return this.bot.sendMessage(chatId, "Извини, но я тебя не понимаю!")
   }
 }
