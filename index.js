b3f0257b8d5d54588e5acf6c3fb155e427f0f2e4
Zhark10 <verygoodman18@yandex.ru>
2021-12-18 00:02:46 +0300

    start bot logic

--- index.js
+++ index.js
@@ -1,64 +1,11 @@
 
-import TelegramApi from "node-telegram-bot-api";
-import { join, dirname } from "path"
-import { Low, JSONFile } from "lowdb"
-import { fileURLToPath } from "url"
+import { MessagesHandler } from "./src/handlers/MessagesHandler.js"
+import { createDatabaseConnection } from "./src/config/db-connection.js"
+import { bot } from "./src/config/bot-initialize.js"
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-let db;
-
-const defaultDatabaseState = {
-  histories: []
+const startBot = async () => {
+  const database = await createDatabaseConnection()
+  return MessagesHandler(bot, database)
 }
 
-export async function createConnection() {
-  const file = join(__dirname, "db.json")
-  const adapter = new JSONFile(file)
-  db = await new Low(adapter)
-  db.data ||= defaultDatabaseState
-  db.read()
-}
-
-export const getConnetcion = () => db
-createConnection()
-
-const token = "5035331349:AAHG-3mOMX0gu-1X0bDA31PxTXGQ1akhn1E"
-
-const bot = new TelegramApi(token, { polling: true })
-
-const start = () => {
-
-  bot.setMyCommands(
-    [
-      { command: "/start", description: "Приветствие" },
-      { command: "/create", description: "Создать историю" },
-    ]
-  )
-
-  bot.on("message", async (msg) => {
-    const text = msg.text
-    const chatId = msg.chat.id
-
-    if (text === "/start") {
-      await bot.sendSticker(chatId, "https://tlgrm.ru/_/stickers/e1f/4c4/e1f4c48a-c808-37a7-a037-bfa58e8b3222/1.webp")
-      return bot.sendMessage(chatId, `Привет, ${msg.from.first_name} ${msg.from.last_name}! Добро пожаловать в "Продолжим?"`)
-    }
-
-    if (text === "/create") {
-      await db.data
-        .histories
-        .push({ title: 'Новая история', text: 'История начинается, когда', id: 1 })
-      return
-    }
-
-    if (text.includes("/continue-")) {
-      const storyId = text.split("-")[1]
-      await bot.sendMessage(chatId, storyId || "Извините, но история не найдена")
-    }
-
-    return bot.sendMessage(chatId, `Я тебя не понимаю, попробуй объяснить еще раз!`)
-  })
-}
-
-start()
+startBot()
