2526f2c93de625f3c30f7384b4387f6c3d66d777
Zhark10 <verygoodman18@yandex.ru>
2021-12-19 00:20:19 +0300

    min changes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { createDatabaseConnection } from "./src/config/db-connection.js"
 import { bot } from "./src/config/bot-initialize.js"
 
 const startBot = async () => {
-  const client = await createDatabaseConnection()
-  return MessagesHandler(bot, client)
+  await createDatabaseConnection()
+  return MessagesHandler(bot)
 }
 
 startBot()
