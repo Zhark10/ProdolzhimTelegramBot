1dc03e49f25bb44fbb82b162213e86de9551df8b
Zhark10 <verygoodman18@yandex.ru>
2021-12-19 00:21:00 +0300

    changed db connection (basic connection to mongoose)

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -1,25 +1,11 @@
-import { MongoClient } from "mongodb"
-import { CONSTANTS } from "./constants.js"
+import mongoose from 'mongoose'
 
 const CONNECTION_URL = 'mongodb+srv://Zhark10:d0uLwSXC7yqg8Vga@clusterforprodolzhim.5stig.mongodb.net/ClusterForProdolzhim?retryWrites=true&w=majority'
-const client = new MongoClient(CONNECTION_URL)
-
-const setCollections = async (client, collections) => {
-  const database = client.db()
-  for (let collectionName of Object.values(collections)) {
-    const thisCollectionAlreadyExist = await database.collection(collectionName)
-    if (!thisCollectionAlreadyExist) {
-      database.createCollection(collectionName)
-    }
-  }
-}
 
 const createDatabaseConnection = async () => {
   try {
-    await client.connect()
-    await setCollections(client, CONSTANTS.DATABASE_COLLECTIONS)
+    await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('Connection is successful')
-    return client
   } catch (error) {
     console.error('Connection error', error)
   }
