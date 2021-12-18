691dd7ad9c08affbe58d378de452e2d02a244a74
Zhark10 <verygoodman18@yandex.ru>
2021-12-18 13:56:21 +0300

    change connection

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -1,27 +1,30 @@
-import { join, dirname } from "path"
-import { Low, JSONFile } from "lowdb"
-import { fileURLToPath } from "url"
+import { MongoClient } from "mongodb"
+import { CONSTANTS } from "./constants.js"
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const CONNECTION_URL = 'mongodb+srv://Zhark10:d0uLwSXC7yqg8Vga@clusterforprodolzhim.5stig.mongodb.net/ClusterForProdolzhim?retryWrites=true&w=majority'
+const client = new MongoClient(CONNECTION_URL)
 
-let database;
-
-const defaultDatabaseState = {
-  histories: []
+const setCollections = async (client, collections) => {
+  const database = client.db()
+  for (let collectionName of Object.values(collections)) {
+    const thisCollectionAlreadyExist = await database.collection(collectionName)
+    if (!thisCollectionAlreadyExist) {
+      database.createCollection(collectionName)
+    }
+  }
 }
 
 const createDatabaseConnection = async () => {
-  const file = join(__dirname, "db.json")
-  const adapter = new JSONFile(file)
-  database = await new Low(adapter)
-  database.data ||= defaultDatabaseState
-  database.read()
-  return database
+  try {
+    await client.connect()
+    await setCollections(client, CONSTANTS.DATABASE_COLLECTIONS)
+    console.log('Connection is successful')
+    return client
+  } catch (error) {
+    console.error('Connection error', error)
+  }
 }
 
-const getConnection = () => database
-
 export {
-  createDatabaseConnection,
-  getConnection,
+  createDatabaseConnection
 }
