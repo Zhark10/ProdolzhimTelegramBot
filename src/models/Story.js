b5eefb5359f929cf65f6c853f30ac68f41fb5f88
Zhark10 <verygoodman18@yandex.ru>
2022-01-06 12:08:43 +0300

    add last part in story model

--- src/models/Story.js
+++ src/models/Story.js
@@ -4,6 +4,7 @@ const StorySchema = mongoose.Schema({
   title: {type: String, required: true},
   text: {type: String, required: true},
   category: {type: String, required: true},
+  lastPart: {type: String, required: true},
   stepSize: {type: Number, required: true},
   limitStory: {type: Number, required: true},
   showAllStory: {type: Boolean, required: true},
