4d572622d6ba51b2b618fd5aa69bc796e9f0ca99
Zhark10 <verygoodman18@yandex.ru>
2022-01-31 01:00:33 +0300

    upd dotenv configs

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,17 @@
+
+import dotenv from 'dotenv'
+import path from 'path';
+const __dirname = path.resolve();
+
 const SEPARATOR_TO_CREATE_UNIQUE_COMMAND = "__"
+
+if (process.env.NODE_ENV) {
+  dotenv.config({
+    path: `${__dirname}/.env.${process.env.NODE_ENV}`
+  })
+} else {
+  dotenv.config()
+}
 
 const TOKENS = {
   MONGO_DB: process.env.MONGO_DB_CONNECTION_URL,
